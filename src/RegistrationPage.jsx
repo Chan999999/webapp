dabb018798773388eaa1661bc5f0cc73b023abcc
Chan999999 <132436535+Chan999999@users.noreply.github.com>
2023-05-30 21:27:16 +0800

    Create RegistrationPage.jsx

---
+++ src/RegistrationPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+const RegistrationPage = () => {
+  return (
+    <div>
+      <h1>Registration Page</h1>
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegistrationPage;
