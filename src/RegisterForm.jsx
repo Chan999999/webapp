fec63376051d27544425234b0599e1f78a304bcf
Chan999999 <132436535+Chan999999@users.noreply.github.com>
2023-05-30 20:57:08 +0800

    Create RegisterForm.jsx

---
+++ src/RegisterForm.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+const RegisterForm = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // 執行註冊邏輯
+    try {
+      const response = await axios.post('/api/users/register', {
+        username,
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+
+    // 清空輸入欄位
+    setUsername('');
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="username">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Register
+      </Button>
+    </Form>
+  );
+};
+
+export default RegisterForm;
