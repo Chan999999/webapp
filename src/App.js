1eec2dec18fb2c300f105055b45b4c0587ddba24
Chan999999 <132436535+Chan999999@users.noreply.github.com>
2023-05-31 16:16:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import {Routes, Route, Link, Navigate} from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import './index.css'
+import './index.css';
 import HomePage from './HomePage';
 import Layout from './Layout';
 
 function App() {
-
   return (
-    <Routes>
-      <Route path='WEBAPP/React/build' element={<Layout/>}>
-        <Route path="homepage" element={<HomePage/>} />
-        <Route path="second" element={<Second/>} />
-        <Route path='' element={<Navigate to='homepage'/>}/>
-      </Route>
-      <Route path='/' element={<Navigate to='/WEBAPP/React/build/homepage'/>}/>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/WEBAPP/React/build" element={<Layout />}>
+          <Route path="homepage" element={<HomePage />} />
+          <Route path="second" element={<Second />} />
+          <Route path="" element={<Navigate to="/WEBAPP/React/build/homepage" />} />
+        </Route>
+        <Route path="/" element={<Navigate to="/WEBAPP/React/build/homepage" />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
 
-function Second(){
-  return(
-    <Link to='/'>回主頁</Link>
+function Second() {
+  return (
+    <Link to="/">回主頁</Link>
   );
 }
