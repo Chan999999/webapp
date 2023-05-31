f482077bc188062a0803fa737d5f46ac3e390bb3
Chan999999 <132436535+Chan999999@users.noreply.github.com>
2023-05-31 11:43:30 +0800

    Create LoginForm.jsx

---
+++ src/LoginForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+  );
+};
+
+export default LoginForm;
