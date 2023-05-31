5b68f1464738286910f4ab48c0e7d170a24ad378
Chan999999 <132436535+Chan999999@users.noreply.github.com>
2023-05-31 16:30:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link, Switch } from 'react-router-dom';
 
 import './App.css';
 import './index.css';
