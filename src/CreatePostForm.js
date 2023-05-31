1e458c2894de404fa9f24ba05683a0825841bbbe
Chan999999 <132436535+Chan999999@users.noreply.github.com>
2023-05-31 16:01:21 +0800

    Create CreatePostForm.js

---
+++ src/CreatePostForm.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const CreatePostForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/posts', { title, content });
+      console.log(response.data);
+      // 清空表單
+      setTitle('');
+      setContent('');
+      // 刷新貼文列表
+      // 可以在此處調用獲取貼文的函數
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Create New Post</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Title:</label>
+          <input type="text" value={title} onChange={handleTitleChange} />
+        </div>
+        <div>
+          <label>Content:</label>
+          <textarea value={content} onChange={handleContentChange} />
+        </div>
+        <button type="submit">Create Post</button>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePostForm;
