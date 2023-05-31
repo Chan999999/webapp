300e01efc4addf9798ccb431ff9c223cabbdc7f5
Chan999999 <132436535+Chan999999@users.noreply.github.com>
2023-05-31 16:28:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,22 @@ import Layout from './Layout';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/WEBAPP/React/build" element={<Layout />} />
+      <Switch>
+        <Route path="/WEBAPP/React/build" element={<Layout />}>
+          <Route path="homepage" element={<HomePage />} />
+          <Route path="second" element={<Second />} />
+          <Route path="" element={<Navigate to="homepage" />} />
+        </Route>
         <Route path="/" element={<Navigate to="/WEBAPP/React/build/homepage" />} />
-      </Routes>
+      </Switch>
     </Router>
   );
 }
 
 export default App;
+
+function Second() {
+  return (
+    <Link to="/">回主頁</Link>
+  );
+}
